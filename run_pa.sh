b9b3eac5bc7ae7fb83ce9543f63fec009fa5acca
mosguinz <mos.guinz@gmail.com>
2024-07-28 17:15:31 -0700

    Open source for parts A and B to check for hardcoding

--- run_pa.sh
+++ run_pa.sh
@@ -1,7 +1,8 @@
 #!/bin/zsh
 setopt nullglob
 
-g++ -std=c++17 -- *[aA]/*.cpp *[aA]/*.h
+files=(*[aA]/*.cpp *[aA]/*.h)
+g++ -std=c++17 -- "${files[@]}"
 
 # No win
 echo "Checking no win"
@@ -14,3 +15,9 @@ echo "Checking X win"
 ./a.out < pa_stdin_xwin.txt > a.txt
 vimdiff -c "set diffopt+=iwhiteall foldlevel=99999" a.txt pa_stdout_xwin.txt
 rm a.out a.txt
+
+# Check for hardcoding fuckery
+for f in "${files[@]}"
+do
+  vim -c "set nomodifiable" "$f"
+done
--- run_pb.sh
+++ run_pb.sh
@@ -1,8 +1,15 @@
 #!/bin/zsh
 setopt nullglob
 
-g++ -std=c++17 -- *[bB]/*.cpp *[bB]/*.h
+files=(*[bB]/*.cpp *[bB]/*.h)
+g++ -std=c++17 -- "${files[@]}"
 
 ./a.out> a.txt
 vimdiff -c "set diffopt+=iwhiteall foldlevel=99999" a.txt pb_stdout.txt
 rm a.out a.txt
+
+# Check for hardcoding fuckery
+for f in "${files[@]}"
+do
+  vim -c "set nomodifiable" "$f"
+done
